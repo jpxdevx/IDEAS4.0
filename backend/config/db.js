8da344a294f1b6b3bb177b6adf2f7cdc9c964b62
John Paul Fernandes <johnpaulfernandes18@gmail.com>
2026-02-28 00:38:52 +0530

    Rollback

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB Connected Successfully");
-  } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/waterMonitoring");
+
+        console.log("✅ MongoDB Connected Successfully");
+    } catch (error) {
+        console.error("❌ MongoDB Connection Failed:", error.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
